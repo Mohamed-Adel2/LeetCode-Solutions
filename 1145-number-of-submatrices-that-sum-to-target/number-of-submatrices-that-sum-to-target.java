df7adc0e18d9ce099bf46d5f7156f32e50db49a4
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-02 09:58:08 +0200

    Time: 154 ms (10.15%) | Memory: 45.2 MB (27.95%) - LeetSync

---
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -0,0 +1,36 @@
+class Main {
+    public static void main(String args[]) {
+        int[][] mat = {{0, 1, 1, 1, 0, 1}, {0, 0, 0, 0, 0, 1}, {0, 0, 1, 0, 0, 1}, {1, 1, 0, 1, 1, 0}, {1, 0, 0, 1, 0, 0}};
+        Solution s = new Solution();
+        System.out.println(s.numSubmatrixSumTarget(mat, 0));
+    }
+
+}
+
+class Solution {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] sum = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                Map<Integer, Integer> freq = new HashMap<>();
+                freq.put(0, 1);
+                int currSum = 0;
+                for (int k = 1; k <= m; k++) {
+                    currSum = sum[j][k] - sum[i - 1][k] - sum[j][0] + sum[i - 1][0];
+                    if (freq.containsKey(currSum - target))
+                        ans += freq.get(currSum - target);
+                    freq.put(currSum, freq.getOrDefault(currSum, 0) + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
