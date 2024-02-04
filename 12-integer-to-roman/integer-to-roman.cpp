d06eda581d0befaf7e9e42cb7ee50bcd8a9f93e6
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-04 11:27:41 +0200

    Time: 18 ms (9.82%) | Memory: 13.2 MB (10.42%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        unordered_map<int, char> base, unit;
-        base[3] = 'D', base[2] = 'L', base[1] = 'V';
-        unit[4] = 'M', unit[3] = 'C', unit[2] = 'X', unit[1] = 'I';
-        int curr = 1;
+        vector<pair<int, string>> v{{1000, "M"},
+                                    {900,  "CM"},
+                                    {500,  "D"},
+                                    {400,  "CD"},
+                                    {100,  "C"},
+                                    {90,   "XC"},
+                                    {50,   "L"},
+                                    {40,   "XL"},
+                                    {10,   "X"},
+                                    {9,    "IX"},
+                                    {5,    "V"},
+                                    {4,    "IV"},
+                                    {1,    "I"}};
         string ans;
-        while (num > 0) {
-            int digit = num % 10;
-            num /= 10;
-            if (digit == 0){
-                ++curr;
-                continue;
+        for (auto &[key, value]: v) {
+            while (num >= key) {
+                ans += value;
+                num -= key;
             }
-            if (digit < 4)
-                ans = oneToThree(digit, unit[curr]) + ans;
-            else if (digit > 4 && digit < 9)
-                ans = fiveToEight(digit - 5, unit[curr], base[curr]) + ans;
-            else if (digit == 4) {
-                ans = base[curr] + ans;
-                ans = unit[curr] + ans;
-            } else {
-                ans = unit[curr + 1] + ans;
-                ans = unit[curr] + ans;
-            }
-            ++curr;
+            if (num == 0)
+                break;
         }
         return ans;
-    }
-
-    string oneToThree(int cnt, char unit) {
-        string ret;
-        for (int i = 0; i < cnt; i++)
-            ret += unit;
-        return ret;
-    }
-
-    string fiveToEight(int cnt, char unit, char base) {
-        string ret;
-        ret += base;
-        for (int i = 0; i < cnt; i++)
-            ret += unit;
-        return ret;
     }
 };
