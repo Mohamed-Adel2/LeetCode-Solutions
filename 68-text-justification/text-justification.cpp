b4cffe0abc465bee368438b4e54ea01004ca3c48
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-07 21:00:40 +0200

    Time: 4 ms (34.50%) | Memory: 9.1 MB (19.47%) - LeetSync

--- 68-text-justification/text-justification.cpp
+++ 68-text-justification/text-justification.cpp
@@ -3,9 +3,8 @@ public:
     vector<string> fullJustify(vector<string> &words, int maxWidth) {
         int n = words.size();
         vector<string> ans;
-        for (int i = 0; i < n; i++) {
-            int j = i;
-            int currSz = 0;
+        for (int i = 0; i < n;) {
+            int j = i, currSz = 0;
             while (j < n && currSz + words[j].size() + (j - i) <= maxWidth) {
                 currSz += words[j].size();
                 ++j;
@@ -13,25 +12,20 @@ public:
             string currLine;
             if (j == n) {
                 for (int word = i; word < j - 1; word++) {
-                    currLine += words[word];
-                    currLine.push_back(' ');
+                    currLine += words[word] + ' ';
                 }
                 currLine += words[j - 1];
-                while (currLine.size() < maxWidth)
-                    currLine.push_back(' ');
-
+                currLine += string(maxWidth - currLine.size(), ' ');
             } else {
                 int fixedSpaces = (maxWidth - currSz) / max(1, (j - i - 1));
                 int extraSpaces = (maxWidth - currSz) % max(1, (j - i - 1));
                 for (int word = i; word < j; word++) {
                     currLine += words[word];
                     if (word < j - 1 || j == i + 1)
-                        for (int space = 0; space < fixedSpaces + (extraSpaces > 0); space++)
-                            currLine.push_back(' ');
-                    extraSpaces--;
+                        currLine += string(fixedSpaces + (extraSpaces-- > 0), ' ');
                 }
             }
-            i = j - 1;
+            i = j;
             ans.push_back(currLine);
         }
         return ans;
