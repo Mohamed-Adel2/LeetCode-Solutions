1d64873a1f7d25c26ef551b4e988088b46e3130d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-08 14:15:22 +0200

    Time: 540 ms (83.37%) | Memory: 554.8 MB (59.31%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,47 @@
+class TrieNode {
+public:
+    TrieNode *child[26];
+    bool endsHere = false;
+};
+
+class WordDictionary {
+public:
+    TrieNode *root;
+
+    WordDictionary() {
+        root = new TrieNode();
+    }
+
+    void addWord(string word) {
+        TrieNode *node = root;
+        for (char c: word) {
+            if (node->child[c - 'a'] == NULL)
+                node->child[c - 'a'] = new TrieNode();
+            node = node->child[c - 'a'];
+        }
+        node->endsHere = true;
+    }
+
+    bool search(string word) {
+        return startFromHere(root, word, 0);
+    }
+
+    bool startFromHere(TrieNode *curr, string &s, int idx) {
+        TrieNode *node = curr;
+        for (int i = idx; i < s.size(); i++) {
+            if (s[i] == '.') {
+                bool canComplete = false;
+                for (int child = 0; child < 26; child++) {
+                    if (node->child[child] != NULL)
+                        canComplete |= startFromHere(node->child[child], s, i + 1);
+                }
+                return canComplete;
+            } else {
+                if (node->child[s[i] - 'a'] == NULL)
+                    return false;
+                node = node->child[s[i] - 'a'];
+            }
+        }
+        return node->endsHere;
+    }
+};
