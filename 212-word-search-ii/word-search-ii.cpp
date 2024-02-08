503f3a9bc19cc129a3453715317c01529e43b303
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-08 16:20:47 +0200

    Time: 337 ms (86.63%) | Memory: 18 MB (38.24%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -29,23 +29,22 @@ class Solution {
 public:
     vector<string> ans;
     int n, m;
-    vector<int> dx, dy;
 
     vector<string> findWords(vector<vector<char>> &board, vector<string> &words) {
         n = board.size(), m = board[0].size();
-        dx = {0, 0, 1, -1};
-        dy = {1, -1, 0, 0};
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
         Trie trie;
         for (int i = 0; i < words.size(); i++)
             trie.insert(words[i], i);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 if (trie.root->child[board[i][j] - 'a'] != NULL)
-                    dfs(board, trie.root->child[board[i][j] - 'a'], i, j);
+                    dfs(board, trie.root->child[board[i][j] - 'a'], i, j, dx, dy);
         return ans;
     }
 
-    void dfs(vector<vector<char>> &board, TrieNode *&node, int row, int col) {
+    void dfs(vector<vector<char>> &board, TrieNode *&node, int row, int col, vector<int> &dx, vector<int> &dy) {
         char c = board[row][col];
         board[row][col] = '#';
         if (node->endsHere) {
@@ -55,7 +54,7 @@ public:
         for (int i = 0; i < 4; i++)
             if (valid(row + dx[i], col + dy[i]) && board[row + dx[i]][col + dy[i]] != '#' &&
                 node->child[board[row + dx[i]][col + dy[i]] - 'a'] != NULL)
-                dfs(board, node->child[board[row + dx[i]][col + dy[i]] - 'a'], row + dx[i], col + dy[i]);
+                dfs(board, node->child[board[row + dx[i]][col + dy[i]] - 'a'], row + dx[i], col + dy[i], dx, dy);
         board[row][col] = c;
     }
 
