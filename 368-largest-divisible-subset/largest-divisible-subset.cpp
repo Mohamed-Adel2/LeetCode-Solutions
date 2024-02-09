ad852ea8099de69a61826b38b70f141c5a99e2f9
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-09 09:02:23 +0200

    Time: 55 ms (14.39%) | Memory: 66.3 MB (5.01%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int> &nums) {
+    vector<int> ans;
+    vector<int> nums;
+    vector<vector<int>> dp;
+    int n;
+
+    vector<int> largestDivisibleSubset(vector<int> &num) {
+        this->nums = num;
         nums.push_back(1);
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        solve(1, 0, nums, dp, n);
-        vector<int> ans;
-        build(1, 0, nums, dp, n, ans);
+        n = nums.size();
+        dp.resize(n + 1, vector<int>(n + 1, -1));
+        solve(1, 0);
+        build(1, 0);
         return ans;
     }
 
-    int solve(int idx, int prv, vector<int> &nums, vector<vector<int>> &dp, int &n) {
+    int solve(int idx, int prv) {
         if (idx == n)
             return 0;
         int &ret = dp[idx][prv];
@@ -19,20 +24,20 @@ public:
             return ret;
         ret = 0;
         if (nums[idx] % nums[prv] == 0)
-            ret = solve(idx + 1, idx, nums, dp, n) + 1;
-        ret = max(ret, solve(idx + 1, prv, nums, dp, n));
+            ret = solve(idx + 1, idx) + 1;
+        ret = max(ret, solve(idx + 1, prv));
         return ret;
     }
 
-    void build(int idx, int prv, vector<int> &nums, vector<vector<int>> &dp, int &n, vector<int> &ans) {
+    void build(int idx, int prv) {
         if (idx == n)
             return;
         int &ret = dp[idx][prv];
-        if (nums[idx] % nums[prv] == 0 && ret == solve(idx + 1, idx, nums, dp, n) + 1) {
+        if (nums[idx] % nums[prv] == 0 && ret == solve(idx + 1, idx) + 1) {
             ans.push_back(nums[idx]);
-            build(idx + 1, idx, nums, dp, n, ans);
+            build(idx + 1, idx);
             return;
         }
-        build(idx + 1, prv, nums, dp, n, ans);
+        build(idx + 1, prv);
     }
 };
