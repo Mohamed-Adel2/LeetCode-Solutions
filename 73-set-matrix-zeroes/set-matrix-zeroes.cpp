65637a0818cb087d5054ce35a0065c1b8fab31b0
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-12 22:32:11 +0200

    Time: 4 ms (96.45%) | Memory: 16.2 MB (18.27%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,7 +5,7 @@ public:
         vector<bool> rows(n), cols(n);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (!matrix[i][j])
+                if (matrix[i][j] == 0)
                     rows[i] = cols[j] = true;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
