37a308e2eedb798fcda98a185a91800c2e917345
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-14 11:32:26 +0200

    Time: 92 ms (98.36%) | Memory: 79.5 MB (40.66%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -26,15 +26,17 @@ public:
         return ret;
     }
 
-    int binarySearch(int &key, int &lo) {
+    int binarySearch(int key, int lo) {
         int l = lo + 1, r = jobs.size() - 1;
         int ans = jobs.size();
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (jobs[mid].first.first >= key)
-                ans = mid, r = mid - 1;
-            else
+            if (jobs[mid].first.first >= key) {
+                ans = mid;
+                r = mid - 1;
+            } else {
                 l = mid + 1;
+            }
         }
         return ans;
     }
