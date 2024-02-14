23acb2a92b9d85171dc7b0123d4c94360ec38012
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-14 10:25:40 +0200

    Time: 38 ms (95.55%) | Memory: 17.7 MB (5.08%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -3,7 +3,7 @@ public:
     int minSteps(string s, string t) {
         vector<int> sFreq(26), tFreq(26);
         for (int i = 0; i < s.size(); ++i)
-            sFreq[s[i] - 'a']++, tFreq[t[i] - 'a']++;
+            ++sFreq[s[i] - 'a'], ++tFreq[t[i] - 'a'];
         int ans = 0;
         for (int i = 0; i < 26; i++)
             if (sFreq[i] > tFreq[i])
