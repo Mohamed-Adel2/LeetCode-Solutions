3b5e8264f5bc1cccf5c804051a9aecc76c245af2
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-16 11:42:07 +0200

    Time: 110 ms (89.92%) | Memory: 66.1 MB (60.95%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int> &arr, int k) {
         unordered_map<int, int> numFreq;
-        vector<int> freq(1e5 + 2);
-        int dist = 0;
-        for (int i = 0; i < arr.size(); ++i) {
-            int &fr = numFreq[arr[i]];
-            if (!fr)
-                ++dist;
-            --freq[fr];
-            ++fr;
-            ++freq[fr];
-        }
-        for (int i = 1; i < freq.size(); ++i) {
-            if (k < i)
+        for (auto &i: arr)
+            ++numFreq[i];
+        vector<int> freq;
+        for (auto &i: numFreq)
+            freq.push_back(i.second);
+        int dist = freq.size();
+        sort(freq.begin(), freq.end());
+        for (int i = 0; i < freq.size(); ++i) {
+            if (k < freq[i])
                 break;
-            dist -= min(k / i, freq[i]);
-            k -= min(i * freq[i], k);
+            --dist;
+            k -= freq[i];
         }
         return dist;
     }
