37374013e920049cac24c37cb54d9c77023eac3b
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-17 11:02:15 +0200

    Time: 129 ms (15.05%) | Memory: 72 MB (7.09%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int furthestBuilding(vector<int> &heights, int bricks, int ladders) {
-        int n = heights.size();
-        int lo = 0, hi = n - 1, ans = 0;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            vector<int> diff;
-            for (int i = 1; i <= mid; i++)
-                if (heights[i] - heights[i - 1] > 0)
-                    diff.push_back(heights[i] - heights[i - 1]);
-            if (valid(diff, bricks, ladders))
-                ans = mid, lo = mid + 1;
-            else
-                hi = mid - 1;
+        multiset<int> ms;
+        int n = heights.size(), ans = 0, bricksSum = 0;
+        for (int i = 1; i < n; ++i) {
+            if (heights[i] - heights[i - 1] >= 0) {
+                ms.insert(heights[i] - heights[i - 1]);
+                if (ms.size() > ladders)
+                    bricksSum += *ms.begin(), ms.erase(ms.begin());
+                if (bricksSum > bricks)
+                    break;
+            }
+            ans = i;
         }
         return ans;
-    }
-
-    bool valid(vector<int> &diff, int bricks, int ladders) {
-        sort(diff.begin(), diff.end());
-        while (!diff.empty()) {
-            if (ladders)
-                --ladders, diff.pop_back();
-            else {
-                if (diff.back() > bricks)
-                    return false;
-                bricks -= diff.back();
-                diff.pop_back();
-            }
-        }
-        return true;
     }
 };
