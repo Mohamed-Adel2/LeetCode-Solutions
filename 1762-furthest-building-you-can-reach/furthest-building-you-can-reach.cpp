6b8eabd6ede55798b04d2d047147b33e811a369a
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-17 11:07:15 +0200

    Time: 70 ms (85.60%) | Memory: 57.2 MB (40.68%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int furthestBuilding(vector<int> &heights, int bricks, int ladders) {
-        multiset<int> ms;
+        priority_queue<int, vector<int>, greater<int>> ms;
         int n = heights.size(), ans = 0, bricksSum = 0;
         for (int i = 1; i < n; ++i) {
             if (heights[i] - heights[i - 1] >= 0) {
-                ms.insert(heights[i] - heights[i - 1]);
+                ms.push(heights[i] - heights[i - 1]);
                 if (ms.size() > ladders)
-                    bricksSum += *ms.begin(), ms.erase(ms.begin());
+                    bricksSum += ms.top(), ms.pop();
                 if (bricksSum > bricks)
                     break;
             }
