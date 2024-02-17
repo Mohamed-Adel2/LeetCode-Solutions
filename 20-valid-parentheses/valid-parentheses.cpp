933cb218e4631621e9366beb16cd9f623819012d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-17 13:06:06 +0200

    Time: 4 ms (11.43%) | Memory: 7.6 MB (15.69%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.size(); ++i) {
             if (s[i] == ')') {
                 if (st.empty() || st.top() != '(')
                     return false;
