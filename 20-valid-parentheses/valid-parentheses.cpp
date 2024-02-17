26edc91dea7c7783cdf20f5d33d5ceae7e2ccb80
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-17 13:04:58 +0200

    Time: 4 ms (11.43%) | Memory: 7.5 MB (27.68%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == ')') {
+                if (st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+            else if (s[i] == '}') {
+                if (st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }
+            else if (s[i] == ']') {
+                if (st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            } else
+                st.push(s[i]);
+        }
+        return st.empty();
+    }
+};
