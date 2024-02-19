821248ef4df2e57830da54653ccc63ffe36f1e60
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-19 05:53:13 +0200

    Time: 0 ms (100.00%) | Memory: 7.1 MB (49.03%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return (n != -2147483648 && __builtin_popcount(n) == 1);
+        return (n > 0 && __builtin_popcount(n) == 1);
     }
 };
