eb8ca9dfe93d120f2dcfa1f69134594769dc5be4
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-21 04:25:21 +0200

    Time: 11 ms (38.56%) | Memory: 8.8 MB (26.31%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -4,7 +4,7 @@ public:
         int anding = 0;
         for (int i = 0; i < 31; ++i)
             if ((left & (1 << i)) && (right & (1 << i)) && (right - left < (1 << i)))
-                anding += (1 << i);
+                anding |= (1 << i);
         return anding;
     }
 };
