a4ec55e1ca59dec02f113534986fa4284dfe9650
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-23 10:29:09 +0200

    Time: 507 ms (5.02%) | Memory: 69 MB (5.18%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     vector<vector<pair<int, int>>> adj;
     int ans = INT32_MAX;
-    vector<vector<bool>> vis;
+    vector<int> vis;
 
     int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
         adj.resize(n);
-        vis.resize(n, vector<bool>(k + 2));
+        vis.resize(n, INT32_MAX);
         for (int i = 0; i < flights.size(); ++i)
             adj[flights[i][0]].emplace_back(flights[i][1], flights[i][2]);
-        dijkstra(src, dst, k);
+        dfs(src, dst, k);
         return (ans == INT32_MAX ? -1 : ans);
     }
 
-    void dijkstra(int src, int dist, int k) {
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> q;
+    void dfs(int src, int dist, int k) {
+        queue<pair<int, pair<int, int>>> q;
         q.push({0, {src, 0}});
         while (!q.empty()) {
-            int cost = q.top().first, node = q.top().second.first, steps = q.top().second.second;
+            int cost = q.front().first, node = q.front().second.first, steps = q.front().second.second;
             q.pop();
-            if (vis[node][steps])
-                continue;
-            vis[node][steps] = true;
+            vis[node] = cost;
             if (node == dist) {
                 ans = min(cost, ans);
                 continue;
@@ -30,7 +28,7 @@ public:
                 continue;
             for (auto i: adj[node]) {
                 int to = i.first, price = i.second;
-                if (!vis[to][steps + 1] && steps <= k)
+                if (vis[to] > cost + price)
                     q.push({cost + price, {to, steps + 1}});
             }
         }
