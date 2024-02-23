49f9f1f0c73e8695f3d2841984ff4f22bb9b5a70
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-23 10:53:26 +0200

    Time: 15 ms (78.73%) | Memory: 15.7 MB (67.51%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-    vector<vector<pair<int, int>>> adj;
-    int ans = INT32_MAX;
-    vector<vector<bool>> vis;
-
     int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
-        adj.resize(n);
-        vis.resize(n, vector<bool>(k + 2));
-        for (int i = 0; i < flights.size(); ++i)
-            adj[flights[i][0]].emplace_back(flights[i][1], flights[i][2]);
-        dijkstra(src, dst, k);
-        return (ans == INT32_MAX ? -1 : ans);
-    }
-
-    void dijkstra(int src, int dist, int k) {
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> q;
-        q.push({0, {src, 0}});
-        while (!q.empty()) {
-            int cost = q.top().first, node = q.top().second.first, steps = q.top().second.second;
-            q.pop();
-            if (vis[node][steps])
-                continue;
-            vis[node][steps] = true;
-            if (node == dist) {
-                ans = min(cost, ans);
-                continue;
-            }
-            if (steps == k + 1)
-                continue;
-            for (auto i: adj[node]) {
-                int to = i.first, price = i.second;
-                if (!vis[to][steps + 1] && steps <= k)
-                    q.push({cost + price, {to, steps + 1}});
-            }
+        vector<int> dis(n, 5e8);
+        vector<int> tmp;
+        dis[src] = 0;
+        for (int i = 0; i <= k; ++i) {
+            tmp = dis;
+            for (auto &edge: flights)
+                tmp[edge[1]] = min(tmp[edge[1]], dis[edge[0]] + edge[2]);
+            dis = tmp;
         }
+        return (dis[dst] == 5e8 ? -1 : dis[dst]);
     }
 };
