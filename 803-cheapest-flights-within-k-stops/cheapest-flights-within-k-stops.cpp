51a8b2034b7d3cac1e8f61bae3791ff04c620ac0
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-23 10:39:08 +0200

    Time: 101 ms (7.53%) | Memory: 20.4 MB (7.74%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
+    vector<vector<pair<int, int>>> adj;
+    int ans = INT32_MAX;
+    vector<vector<bool>> vis;
 
     int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        vector<int> dis(n, INT32_MAX);
-        int ans = INT32_MAX;
+        adj.resize(n);
+        vis.resize(n, vector<bool>(k + 2));
         for (int i = 0; i < flights.size(); ++i)
             adj[flights[i][0]].emplace_back(flights[i][1], flights[i][2]);
-        queue<pair<int, pair<int, int>>> q;
+        dijkstra(src, dst, k);
+        return (ans == INT32_MAX ? -1 : ans);
+    }
+
+    void dijkstra(int src, int dist, int k) {
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> q;
         q.push({0, {src, 0}});
         while (!q.empty()) {
-            int cost = q.front().first, node = q.front().second.first, steps = q.front().second.second;
+            int cost = q.top().first, node = q.top().second.first, steps = q.top().second.second;
             q.pop();
-            dis[node] = cost;
-            if (steps > k + 1)
-                break;
-            if (node == dst) {
+            if (vis[node][steps])
+                continue;
+            vis[node][steps] = true;
+            if (node == dist) {
                 ans = min(cost, ans);
                 continue;
             }
+            if (steps == k + 1)
+                continue;
             for (auto i: adj[node]) {
                 int to = i.first, price = i.second;
-                if (dis[to] > cost + price)
+                if (!vis[to][steps + 1] && steps <= k)
                     q.push({cost + price, {to, steps + 1}});
             }
         }
-        return (ans == INT32_MAX ? -1 : ans);
     }
 };
