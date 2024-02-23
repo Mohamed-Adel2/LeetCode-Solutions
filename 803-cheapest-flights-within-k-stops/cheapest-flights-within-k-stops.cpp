1825950b48c65cf73527c7fa24a70956e084142d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-23 10:32:06 +0200

    Time: 501 ms (5.02%) | Memory: 69.2 MB (5.18%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -9,23 +9,23 @@ public:
         vis.resize(n, INT32_MAX);
         for (int i = 0; i < flights.size(); ++i)
             adj[flights[i][0]].emplace_back(flights[i][1], flights[i][2]);
-        dfs(src, dst, k);
+        bfs(src, dst, k);
         return (ans == INT32_MAX ? -1 : ans);
     }
 
-    void dfs(int src, int dist, int k) {
+    void bfs(int src, int dist, int k) {
         queue<pair<int, pair<int, int>>> q;
         q.push({0, {src, 0}});
         while (!q.empty()) {
             int cost = q.front().first, node = q.front().second.first, steps = q.front().second.second;
             q.pop();
+            if (steps > k + 1)
+                break;
             vis[node] = cost;
             if (node == dist) {
                 ans = min(cost, ans);
                 continue;
             }
-            if (steps == k + 1)
-                continue;
             for (auto i: adj[node]) {
                 int to = i.first, price = i.second;
                 if (vis[to] > cost + price)
