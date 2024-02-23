8706a69623a6b58b6abdfc72c9a27b6d46a5af94
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-23 10:38:53 +0200

    Time: 527 ms (5.02%) | Memory: 69.3 MB (5.18%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    vector<vector<pair<int, int>>> adj;
-    int ans = INT32_MAX;
-    vector<int> vis;
 
     int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
-        adj.resize(n);
-        vis.resize(n, INT32_MAX);
+        vector<vector<pair<int, int>>> adj(n);
+        vector<int> dis(n, INT32_MAX);
+        int ans = INT32_MAX;
         for (int i = 0; i < flights.size(); ++i)
             adj[flights[i][0]].emplace_back(flights[i][1], flights[i][2]);
-        bfs(src, dst, k);
-        return (ans == INT32_MAX ? -1 : ans);
-    }
-
-    void bfs(int src, int dist, int k) {
         queue<pair<int, pair<int, int>>> q;
         q.push({0, {src, 0}});
         while (!q.empty()) {
             int cost = q.front().first, node = q.front().second.first, steps = q.front().second.second;
             q.pop();
+            dis[node] = cost;
             if (steps > k + 1)
                 break;
-            vis[node] = cost;
-            if (node == dist) {
+            if (node == dst) {
                 ans = min(cost, ans);
                 continue;
             }
             for (auto i: adj[node]) {
                 int to = i.first, price = i.second;
-                if (vis[to] > cost + price)
+                if (dis[to] > cost + price)
                     q.push({cost + price, {to, steps + 1}});
             }
         }
+        return (ans == INT32_MAX ? -1 : ans);
     }
 };
