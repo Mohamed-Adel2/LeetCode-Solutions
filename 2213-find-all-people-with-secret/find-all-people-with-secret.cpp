0dac264ae5dbaa32292baafb3927877e74c3a210
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-24 19:44:56 +0200

    Time: 471 ms (69.67%) | Memory: 173.2 MB (59.43%) - LeetSync

---
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<vector<pair<int, int>>> adj;
+
+    vector<int> findAllPeople(int n, vector<vector<int>> &meetings, int firstPerson) {
+        adj.resize(n);
+        for (int i = 0; i < meetings.size(); ++i){
+            adj[meetings[i][0]].push_back({meetings[i][1], meetings[i][2]});
+            adj[meetings[i][1]].push_back({meetings[i][0], meetings[i][2]});
+        }
+        dijkstra(firstPerson, n);
+        return ans;
+    }
+
+    void dijkstra(int start, int n) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+        q.push({0, start});
+        q.push({0, 0});
+        vector<bool> vis(n);
+        while (!q.empty()) {
+            int node = q.top().second, time = q.top().first;
+            q.pop();
+            if (vis[node])
+                continue;
+            ans.push_back(node);
+            vis[node] = true;
+            for (auto i: adj[node]) {
+                if (i.second >= time)
+                    q.push({i.second, i.first});
+            }
+        }
+    }
+};
