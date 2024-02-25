4db6050bffa718764d22de9bf50f2bb74d1aba58
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-25 16:37:54 +0200

    Time: 16 ms (70.28%) | Memory: 16.8 MB (55.86%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    bool valid = true;
-
     bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> vis(numCourses);
+        vector<int> inDegree(numCourses);
         for (int i = 0; i < prerequisites.size(); ++i)
-            adj[prerequisites[i][1]].emplace_back(prerequisites[i][0]);
+            adj[prerequisites[i][1]].emplace_back(prerequisites[i][0]), ++inDegree[prerequisites[i][0]];
+        queue<int> topSort;
         for (int i = 0; i < numCourses; ++i)
-            if (!vis[i])
-                dfs(i, adj, vis);
-        return valid;
-    }
-
-    void dfs(int node, vector<vector<int>> &adj, vector<int> &vis) {
-        vis[node] = 2;
-        for (auto &i: adj[node]) {
-            if (!valid)
-                return;
-            if (vis[i] == 2) {
-                valid = false;
-                return;
-            } else if (vis[i])
-                continue;
-            dfs(i, adj, vis);
+            if (!inDegree[i])
+                topSort.push(i);
+        int done = 0;
+        while (!topSort.empty()) {
+            int node = topSort.front();
+            topSort.pop(), ++done;
+            for (auto &i: adj[node]) {
+                --inDegree[i];
+                if (!inDegree[i])
+                    topSort.push(i);
+            }
         }
-        --vis[node];
+        return done == numCourses;
     }
 };
