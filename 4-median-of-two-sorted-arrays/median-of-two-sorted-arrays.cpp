553cc3550a21621ccf7a11ed182a9004eb1ee5e4
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-26 20:13:38 +0200

    Time: 35 ms (24.94%) | Memory: 94.4 MB (57.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,11 +1,14 @@
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
         int sz1 = nums1.size(), sz2 = nums2.size();
         if (!sz1)
-            return (sz2 & 1 ? (double) nums2[sz2 / 2] : (double) (nums2[sz2 / 2] + nums2[sz2 / 2 - 1]) / 2);
+            return (sz2 & 1 ? (double) nums2[sz2 / 2] :
+                    (double) (nums2[sz2 / 2] + nums2[sz2 / 2 - 1]) / 2);
         if (!sz2)
-            return (sz1 & 1 ? (double) nums1[sz1 / 2] : (double) (nums1[sz1 / 2] + nums1[sz1 / 2 - 1]) / 2);
+            return (sz1 & 1 ? (double) nums1[sz1 / 2] :
+                    (double) (nums1[sz1 / 2] + nums1[sz1 / 2 - 1]) / 2);
         int sz = sz1 + sz2, target = (sz + 1) / 2;
         int lo = max(0, target - sz2 - 1), hi = min(target, sz1) - 1;
         int left = -1, right = -1;
@@ -23,12 +26,28 @@ public:
         }
         if (left == -1 && right == -1)
             left = 0, right = target;
-        int a1 = (left - 1 >= 0 ? nums1[left - 1] : -10000000), a2 = (right - 1 >= 0 ? nums2[right - 1] : -10000000);
-        int b1 = (left < sz1 ? nums1[left] : 10000000), b2 = (right < sz2 ? nums2[right] : 10000000);
-        if (sz & 1)
-            return max(a1, a2);
-        else
-            return (double) (max(a1, a2) + min(b1, b2)) / 2;
+        if (sz & 1) {
+            if (left == 0)
+                return nums2[target - 1];
+            else if (right == 0)
+                return nums1[target - 1];
+            else
+                return max(nums1[left - 1], nums2[right - 1]);
+        } else {
+            if (left == 0) {
+                int n1 = nums1[0];
+                int n2 = (right < sz2 ? nums2[right] : 10000000);
+                return (double) (nums2[right - 1] + min(n1, n2)) / 2;
+            } else if (right == 0) {
+                int n1 = nums2[0];
+                int n2 = (left < sz1 ? nums1[left] : 10000000);
+                return (double) (nums1[left - 1] + min(n1, n2)) / 2;
+            } else {
+                int n1 = (left < sz1 ? nums1[left] : 10000000);
+                int n2 = (right < sz2 ? nums2[right] : 10000000);
+                return (double) (max(nums1[left - 1], nums2[right - 1]) + min(n1, n2)) / 2;
+            }
+        }
     }
 
     bool valid(int sz, int idx) {
