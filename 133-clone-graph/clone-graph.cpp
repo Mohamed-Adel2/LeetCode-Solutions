c16e756119ffcb114f7adf7c0fef2e73cb1fce9d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-26 21:50:27 +0200

    Time: 3 ms (75.74%) | Memory: 11.9 MB (7.14%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_map<int, Node *> mp;
+    unordered_map<int, bool> vis;
+
+    Node *cloneGraph(Node *node) {
+        if (!node)
+            return NULL;
+        Node *newNode = new Node(node->val);
+        mp[node->val] = newNode;
+        vis[node->val] = true;
+        for (auto &i: node->neighbors) {
+            if (!vis[i->val])
+                newNode->neighbors.push_back(cloneGraph(i));
+            else
+                newNode->neighbors.push_back(mp[i->val]);
+        }
+        return newNode;
+    }
+};
