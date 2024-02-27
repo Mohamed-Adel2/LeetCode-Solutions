00c2cae5f8345862b09676665025f69ffe4c41d1
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-27 06:08:47 +0200

    Time: 3 ms (95.23%) | Memory: 18.9 MB (70.17%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int ans = 0;
+
+    int diameterOfBinaryTree(TreeNode *root) {
+        solve(root);
+        return ans - 1;
+    }
+
+    int solve(TreeNode *node) {
+        if (!node)
+            return 0;
+        int left = solve(node->left), right = solve(node->right);
+        ans = max(ans, left + right + 1);
+        return max(left, right) + 1;
+    }
+};
