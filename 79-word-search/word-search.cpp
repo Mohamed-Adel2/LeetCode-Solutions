e44dc545310e8d40bc27804073e27f8895816717
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-29 08:36:21 +0200

    Time: 244 ms (83.40%) | Memory: 9.5 MB (67.36%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -7,15 +7,14 @@ public:
         bool exist = false;
         n = board.size(), m = board[0].size();
         dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
-        string curr;
         for (int row = 0; row < n; ++row) {
             for (int col = 0; col < m; ++col) {
+                if (board[row][col] != word[0])
+                    continue;
                 char ch = board[row][col];
-                curr.push_back(board[row][col]);
                 board[row][col] = '-';
-                solve(board, exist, word, curr, word.size() - 1, row, col);
+                solve(board, exist, word, 1, row, col);
                 board[row][col] = ch;
-                curr.pop_back();
                 if (exist)
                     return true;
             }
@@ -23,20 +22,16 @@ public:
         return false;
     }
 
-    void solve(vector<vector<char>> &board, bool &exist, string &word, string &curr, int sz, int row, int col) {
-        if (sz == 0) {
-            exist = (word == curr);
-            return;
-        }
+    void solve(vector<vector<char>> &board, bool &exist, string &word, int idx, int row, int col) {
+        if (idx == word.size())
+            return void(exist = true);
         for (int i = 0; i < 4; ++i) {
             int newRow = row + dx[i], newCol = col + dy[i];
-            if (valid(newRow, newCol) && board[newRow][newCol] != '-') {
-                curr.push_back(board[newRow][newCol]);
+            if (valid(newRow, newCol) && board[newRow][newCol] == word[idx]) {
                 char ch = board[newRow][newCol];
                 board[newRow][newCol] = '-';
-                solve(board, exist, word, curr, sz - 1, newRow, newCol);
+                solve(board, exist, word, idx + 1, newRow, newCol);
                 board[newRow][newCol] = ch;
-                curr.pop_back();
                 if (exist)
                     return;
             }
@@ -44,6 +39,6 @@ public:
     }
 
     bool valid(int row, int col) {
-        return (row >= 0 && row < n && col >= 0 && col < m);
+        return !(row < 0 || row >= n || col < 0 || col >= m);
     }
 };
