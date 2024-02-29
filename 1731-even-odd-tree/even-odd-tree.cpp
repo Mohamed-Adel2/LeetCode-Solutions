8eede4ac764a2e481320bbcd0d3f05b0d60523f7
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-29 04:58:43 +0200

    Time: 190 ms (85.12%) | Memory: 152.1 MB (44.21%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode *root) {
+        queue<pair<TreeNode *, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            pair<TreeNode *, int> top = q.front();
+            q.pop();
+            if (((top.second & 1) && (top.first->val & 1)) ||
+                ((top.second & 1) && !q.empty() && q.front().second == top.second && q.front().first->val <= top.first->val))
+                return false;
+            if ((!(top.second & 1) && !(top.first->val & 1)) ||
+                (!(top.second & 1) && !q.empty() && q.front().second == top.second && q.front().first->val >= top.first->val))
+                return false;
+            if (top.first->right)
+                q.push({top.first->right, top.second + 1});
+            if (top.first->left)
+                q.push({top.first->left, top.second + 1});
+        }
+        return true;
+    }
+};
