eed249ffd950caf1b1cfae6f6eb96048a8924383
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-02-29 08:31:54 +0200

    Time: 1177 ms (12.86%) | Memory: 9.6 MB (62.40%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    vector<vector<char>> board;
     int n, m;
     vector<int> dx, dy;
 
-    bool exist(vector<vector<char>> &b, string word) {
-        board = b;
+    bool exist(vector<vector<char>> &board, string word) {
         bool exist = false;
-        n = b.size(), m = b[0].size();
+        n = board.size(), m = board[0].size();
         dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
         string curr;
         for (int row = 0; row < n; ++row) {
@@ -15,7 +13,7 @@ public:
                 char ch = board[row][col];
                 curr.push_back(board[row][col]);
                 board[row][col] = '-';
-                solve(exist, word, curr, word.size() - 1, row, col);
+                solve(board, exist, word, curr, word.size() - 1, row, col);
                 board[row][col] = ch;
                 curr.pop_back();
                 if (exist)
@@ -25,18 +23,18 @@ public:
         return false;
     }
 
-    void solve(bool &exist, string &word, string &curr, int sz, int row, int col) {
+    void solve(vector<vector<char>> &board, bool &exist, string &word, string &curr, int sz, int row, int col) {
         if (sz == 0) {
             exist = (word == curr);
             return;
         }
         for (int i = 0; i < 4; ++i) {
             int newRow = row + dx[i], newCol = col + dy[i];
-            if (valid(newRow, newCol)) {
+            if (valid(newRow, newCol) && board[newRow][newCol] != '-') {
                 curr.push_back(board[newRow][newCol]);
                 char ch = board[newRow][newCol];
                 board[newRow][newCol] = '-';
-                solve(exist, word, curr, sz - 1, newRow, newCol);
+                solve(board, exist, word, curr, sz - 1, newRow, newCol);
                 board[newRow][newCol] = ch;
                 curr.pop_back();
                 if (exist)
@@ -46,6 +44,6 @@ public:
     }
 
     bool valid(int row, int col) {
-        return (row >= 0 && row < n && col >= 0 && col < m && board[row][col] != '-');
+        return (row >= 0 && row < n && col >= 0 && col < m);
     }
 };
