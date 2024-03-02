c42289c7e40def83db39ad1038a90b9c25316e8b
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-02 09:16:39 +0200

    Time: 25 ms (53.25%) | Memory: 29.4 MB (28.90%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int> &nums) {
-        vector<int> pos, neg;
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] > 0)
-                pos.emplace_back(nums[i]);
+        int negPtr = 0, posPtr = nums.size() - 1;
+        vector<int> ret;
+        while (negPtr <= posPtr) {
+            if (abs(nums[negPtr]) > abs(nums[posPtr]))
+                ret.emplace_back(nums[negPtr] * nums[negPtr++]);
             else
-                neg.emplace_back(-nums[i]);
+                ret.emplace_back(nums[posPtr] * nums[posPtr--]);
         }
-        int posPtr = 0, negPtr = neg.size() - 1, numsPtr = 0, posSz = pos.size();
-        while (posPtr < posSz && negPtr >= 0) {
-            if (pos[posPtr] < neg[negPtr])
-                nums[numsPtr++] = pos[posPtr] * pos[posPtr++];
-            else
-                nums[numsPtr++] = neg[negPtr] * neg[negPtr--];
-        }
-        while (posPtr < posSz)
-            nums[numsPtr++] = pos[posPtr] * pos[posPtr++];
-        while (negPtr >= 0)
-            nums[numsPtr++] = neg[negPtr] * neg[negPtr--];
-        return nums;
+        reverse(ret.begin(), ret.end());
+        return ret;
     }
 };
