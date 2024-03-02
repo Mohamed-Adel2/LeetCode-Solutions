c2ac7aaac840ffa942ef61944d109e50fa01bf48
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-02 09:12:40 +0200

    Time: 26 ms (47.82%) | Memory: 29.9 MB (13.09%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -9,14 +9,14 @@ public:
             else
                 neg.emplace_back(-nums[i]);
         }
-        int posPtr = 0, negPtr = neg.size() - 1, numsPtr = 0;
-        while (posPtr < pos.size() && negPtr >= 0) {
+        int posPtr = 0, negPtr = neg.size() - 1, numsPtr = 0, posSz = pos.size();
+        while (posPtr < posSz && negPtr >= 0) {
             if (pos[posPtr] < neg[negPtr])
                 nums[numsPtr++] = pos[posPtr] * pos[posPtr++];
             else
                 nums[numsPtr++] = neg[negPtr] * neg[negPtr--];
         }
-        while (posPtr < pos.size())
+        while (posPtr < posSz)
             nums[numsPtr++] = pos[posPtr] * pos[posPtr++];
         while (negPtr >= 0)
             nums[numsPtr++] = neg[negPtr] * neg[negPtr--];
