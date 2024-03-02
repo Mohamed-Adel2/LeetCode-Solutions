695248852d98276c00800c31b6a0113c0d9c09ea
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-02 09:48:58 +0200

    Time: 5 ms (51.44%) | Memory: 18.3 MB (52.55%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -12,16 +12,10 @@ public:
             head = list2, list2 = list2->next;
         node = head;
         while(list1 && list2){
-            if(list1->val <= list2->val){
-                node->next = list1; 
-                list1 = list1->next; 
-                node = node->next;
-            }
-            else{
-                node->next = list2; 
-                list2 = list2->next; 
-                node = node->next;
-            }
+            if(list1->val <= list2->val)
+                node->next = list1, list1 = list1->next, node = node->next;
+            else
+                node->next = list2, list2 = list2->next, node = node->next;
         }
         if(list2)
             node->next = list2;
