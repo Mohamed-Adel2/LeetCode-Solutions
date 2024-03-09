3d6dceeacc8b2a4ee685915896477be2501b0e93
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-09 21:19:03 +0200

    Time: 4 ms (49.41%) | Memory: 13.4 MB (23.24%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int> &nums) {
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         vector<int> curr;
         map<vector<int>, bool> vis;
         sort(nums.begin(), nums.end());
-        solve(ans, curr, nums, vis, 0);
-        return ans;
+        solve(ans, curr, nums, 0);
+        vector<vector<int>> ret;
+        for(auto i : ans)
+            ret.push_back(i);
+        return ret;
     }
 
-    void solve(vector<vector<int>> &ans, vector<int> &curr, vector<int> &nums, map<vector<int>, bool> &vis,
-               int idx) {
-        if (idx == nums.size()) {
-            if (!vis[curr])
-                ans.emplace_back(curr), vis[curr] = true;
-            return;
-        }
-        solve(ans, curr, nums, vis, idx + 1);
+    void solve(set<vector<int>> &ans, vector<int> &curr, vector<int> &nums, int idx) {
+        if (idx == nums.size())
+            return void(ans.insert(curr));
+        solve(ans, curr, nums, idx + 1);
         curr.emplace_back(nums[idx]);
-        solve(ans, curr, nums, vis, idx + 1);
+        solve(ans, curr, nums, idx + 1);
         curr.pop_back();
     }
 };
