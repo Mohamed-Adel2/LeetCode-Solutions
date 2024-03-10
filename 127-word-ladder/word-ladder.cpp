fbf90d5dab3b48a54b5e25db073e7206d0888c4c
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-10 13:09:57 +0200

    Time: 68 ms (61.63%) | Memory: 20.4 MB (37.04%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     int ladderLength(string start, string end, vector<string> &list) {
         queue<pair<string, int>> q;
-        unordered_set<string> freq(list.begin(), list.end());
+        unordered_map<string, bool> freq;
+        for (auto &el: list)
+            freq[el] = true;
+        for (int i = 0; i < list.size(); ++i)
+            freq[list[i]] = true;
+        vector<bool> vis(list.size());
         q.push({start, 0});
         while (!q.empty()) {
             string curr = q.front().first;
