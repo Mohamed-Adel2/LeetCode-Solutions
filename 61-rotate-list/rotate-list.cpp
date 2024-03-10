b5a1c0cb8d34711e47009e3949c742e5510f4639
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-10 10:01:35 +0200

    Time: 0 ms (100.00%) | Memory: 15.2 MB (22.83%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if(!head)
+            return head;
+        int sz = 0;
+        ListNode *node = head, *tail;
+        while (node)
+            tail = node, ++sz, node = node->next;
+        k %= sz;
+        if (k == 0)
+            return head;
+        node = head;
+        int cnt = 0;
+        tail->next = head;
+        while (node) {
+            if (cnt == sz - k - 1) {
+                tail = node->next;
+                node->next = NULL;
+                break;
+            }
+            node = node->next;
+            ++cnt;
+        }
+        return tail;
+    }
+};
