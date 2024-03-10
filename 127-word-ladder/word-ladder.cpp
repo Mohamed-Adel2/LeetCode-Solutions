4f269c7baab40623506dfbbf6ef454e626beb733
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-10 13:08:28 +0200

    Time: 65 ms (66.04%) | Memory: 19.3 MB (70.96%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int ladderLength(string start, string end, vector<string> &list) {
         queue<pair<string, int>> q;
-        vector<bool> vis(list.size());
+        unordered_set<string> freq(list.begin(), list.end());
         q.push({start, 0});
         while (!q.empty()) {
             string curr = q.front().first;
@@ -10,23 +10,18 @@ public:
             q.pop();
             if (curr == end)
                 return dist + 1;
-            for (int i = 0; i < list.size(); ++i) {
-                if (!vis[i] && differByOne(curr, list[i]))
-                    vis[i] = true, q.push({list[i], dist + 1});
+            for (int i = 0; i < curr.size(); ++i) {
+                for (char ch = 'a'; ch <= 'z'; ++ch) {
+                    if (ch == curr[i])
+                        continue;
+                    char tmp = curr[i];
+                    curr[i] = ch;
+                    if (freq.find(curr) != freq.end())
+                        q.push({curr, dist + 1}), freq.erase(curr);
+                    curr[i] = tmp;
+                }
             }
         }
         return 0;
-    }
-
-    bool differByOne(string &s1, string &s2) {
-        int cnt = 0;
-        for (int i = 0; i < s1.size(); ++i) {
-            if (s1[i] != s2[i]) {
-                ++cnt;
-                if (cnt == 2)
-                    return false;
-            }
-        }
-        return true;
     }
 };
