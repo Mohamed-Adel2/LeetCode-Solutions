c126b2911d2279c27139799466ad8aa746f1cb77
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-10 09:26:46 +0200

    Time: 5 ms (49.43%) | Memory: 14.1 MB (7.77%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        unordered_map<int, int> freq;
+        for (auto &i: nums1)
+            freq[i] = 1;
+        for (auto &i: nums2)
+            if (freq[i] == 1)
+                freq[i] = 2;
+        vector<int> ans;
+        for (auto &el: freq)
+            if (el.second == 2)
+                ans.emplace_back(el.first);
+        return ans;
+    }
+};
