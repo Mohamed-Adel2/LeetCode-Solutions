39782066cf084b911a1acc92b2b259e53df21cdb
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-11 04:51:31 +0200

    Time: 2 ms (65.07%) | Memory: 11.8 MB (81.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -3,23 +3,22 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> ret;
         string curr;
-        solve(ret, curr, 2 * n, 0);
+        solve(ret, curr, 2 * n, n, n);
         return ret;
     }
 
-    void solve(vector<string>& ret, string& curr, int idx, int open){
-        if(idx == 0){
-            if(!open)
-                ret.push_back(curr);
-            return;
-        }
-        if(open){
+    void solve(vector<string>& ret, string& curr, int idx, int open, int close){
+        if(idx == 0)
+            return void(ret.push_back(curr));
+        if(open < close){
             curr.push_back(')');
-            solve(ret, curr, idx - 1, open - 1);
+            solve(ret, curr, idx - 1, open, close - 1);
             curr.pop_back();
         }
+        if(open){
         curr.push_back('(');
-        solve(ret, curr, idx - 1, open + 1);
+        solve(ret, curr, idx - 1, open - 1, close);
         curr.pop_back();
+        }
     }
 };
