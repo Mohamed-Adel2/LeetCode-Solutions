96ded2ea85ef27c3318192ca0a9cb6fd122bdbe6
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-11 04:35:07 +0200

    Time: 3 ms (50.27%) | Memory: 7.3 MB (78.20%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        int freq[26] = {};
+        for (auto &ch: s)
+            ++freq[ch - 'a'];
+        int idx = 0;
+        for (auto &ch: order) {
+            for (int i = 0; i < freq[ch - 'a']; ++i)
+                s[idx++] = ch;
+            freq[ch - 'a'] = 0;
+        }
+        for (int i = 0; i < 26; ++i)
+            for (int j = 0; j < freq[i]; ++j)
+                s[idx++] = (char) (i + 'a');
+        return s;
+    }
+};
