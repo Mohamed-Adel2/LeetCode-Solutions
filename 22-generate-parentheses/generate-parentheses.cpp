88ad7c09ac1f8bd83fac84799077fda1604638aa
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-11 05:22:04 +0200

    Time: 0 ms (100.00%) | Memory: 11.7 MB (89.47%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -8,17 +8,17 @@ public:
     }
 
     void solve(vector<string>& ret, string& curr, int idx, int open, int close){
-        if(idx == 0)
+        if(!idx)
             return void(ret.push_back(curr));
         if(open < close){
             curr.push_back(')');
             solve(ret, curr, idx - 1, open, close - 1);
             curr.pop_back();
         }
-        if(open > 0){
-        curr.push_back('(');
-        solve(ret, curr, idx - 1, open - 1, close);
-        curr.pop_back();
+        if(open){
+            curr.push_back('(');
+            solve(ret, curr, idx - 1, open - 1, close);
+            curr.pop_back();
         }
     }
 };
