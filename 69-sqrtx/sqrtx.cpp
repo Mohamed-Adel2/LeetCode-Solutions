2a6bb046abbe5f26c2a46c152495107522befcfc
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-14 21:30:40 +0200

    Time: 2 ms (50.78%) | Memory: 7.4 MB (73.02%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i;
-        for (i = 0; i * i <= x; ++i);
-        return i - 1;
+        long long lo = 0, hi = x;
+        int ans = 0;
+        while (lo <= hi) {
+            long long mid = (lo + hi) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                lo = mid + 1;
+            } else
+                hi = mid - 1;
+        }
+        return ans;
     }
 };
