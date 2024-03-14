9cb8ee3c5d2a37a5a6865f1a4c9cad0438e85d53
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-14 09:31:47 +0200

    Time: 65 ms (21.27%) | Memory: 46.7 MB (10.17%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal) {
+        unordered_map<int, int> oneFreq, zeroFreq;
+        int currSum = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!nums[i])
+                ++zeroFreq[currSum];
+            else
+                ++oneFreq[++currSum];
+            ans += zeroFreq[currSum - goal] + oneFreq[currSum - (goal - 1)];
+        }
+        return ans;
+    }
+};
