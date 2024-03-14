03da154620143731ee582348db8a31e05747d8ac
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-14 21:42:32 +0200

    Time: 0 ms (100.00%) | Memory: 8 MB (43.16%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,7 +5,7 @@ public:
             return 1;
         if (n < 0)
             return (1 / myPow(x, -(n + 1))) * (1 / x);
-        double val = myPow(x, n >> 1);
+        double val = myPow(x, n / 2);
         return val * val * (n & 1 ? x : 1);
     }
 };
