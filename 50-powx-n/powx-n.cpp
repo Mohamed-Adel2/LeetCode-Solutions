b9d6ce5184503c79967edac4d7a9da3bfdda5c31
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-14 21:40:54 +0200

    Time: 3 ms (38.60%) | Memory: 8 MB (43.16%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return (1 / myPow(x, -(n + 1))) * (1 / x);
+        double val = myPow(x, n >> 1);
+        return val * val * (n & 1 ? x : 1);
+    }
+};
