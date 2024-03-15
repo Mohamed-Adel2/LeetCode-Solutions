f1ec60c770dc4e0b08b243d94d1a934ca469e748
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-15 10:50:53 +0200

    Time: 0 ms (100.00%) | Memory: 11.8 MB (78.75%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int singleNumber(vector<int> &nums) {
         vector<int> bits(32);
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < 32; ++j) {
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = 0; j < 32; ++j)
                 if (nums[i] & (1 << j))
                     ++bits[j];
-            }
-        }
         int ans = 0;
         for (int i = 0; i < 32; ++i)
             if (bits[i] % 3 != 0)
-                ans += (1 << i);
+                ans |= (1 << i);
         return ans;
     }
 };
