67c814a39b88bfaa749111f3575f08696d1134f9
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-15 10:49:40 +0200

    Time: 7 ms (54.65%) | Memory: 11.7 MB (78.75%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -11,7 +11,7 @@ public:
         int ans = 0;
         for (int i = 0; i < 32; ++i)
             if (bits[i] % 3 != 0)
-                ans |= (1 << i);
+                ans += (1 << i);
         return ans;
     }
 };
