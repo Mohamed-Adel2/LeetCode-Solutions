96183989640dd0fd5c5f0f876cd537f5e9cacdc6
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-16 05:20:36 +0200

    Time: 74 ms (88.95%) | Memory: 87.7 MB (53.36%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int findMaxLength(vector<int> &nums) {
-        int n = nums.size(), ans = 0;
-        vector<vector<int>> prefix(n + 1, vector<int>(2));
+        int n = nums.size(), ans = 0, curr = 0;
         unordered_map<int, int> diff;
         diff[0] = 0;
         for (int i = 1; i <= n; ++i) {
-            prefix[i][0] = prefix[i - 1][0] + (nums[i - 1] == 0);
-            prefix[i][1] = prefix[i - 1][1] + (nums[i - 1] == 1);
-            int curr = prefix[i][0] - prefix[i][1];
+            curr += (nums[i - 1] == 1) - (nums[i - 1] == 0);
             if (diff.count(curr))
                 ans = max(ans, i - diff[curr]);
             else
