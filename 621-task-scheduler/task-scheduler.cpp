380ac5c127c444c41cc9ce8e64a4b14b35e85a6a
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-19 06:10:34 +0200

    Time: 46 ms (88.70%) | Memory: 38 MB (80.77%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char> &tasks, int n) {
-        int sz = tasks.size();
         vector<int> freq(26);
-        vector<int> lst(26, -101);
-        for (auto &el: tasks)
+        int mxFreq = 0, cnt = 0;
+        for (auto &el: tasks) {
             ++freq[el - 'A'];
-        int ans = 0;
-        for (int i = 1; i <= sz; ++i) {
-            ++ans;
-            int mxAvailable = 0, mxUnavailable = 0;
-            char available, unavailable;
-            for (int j = 0; j < 26; ++j) {
-                if (freq[j] > mxAvailable && ans - lst[j] - 1 >= n) {
-                    mxAvailable = freq[j];
-                    available = (j + 'A');
-                } else if (freq[j] > mxUnavailable && ans - lst[j] - 1 < n) {
-                    mxUnavailable = freq[j];
-                    unavailable = (j + 'A');
-                }
-            }
-            if (mxAvailable != 0) {
-                --freq[available - 'A'];
-                lst[available - 'A'] = ans;
-            } else {
-                --freq[unavailable - 'A'];
-                ans += n - (ans - lst[unavailable - 'A'] - 1);
-                lst[unavailable - 'A'] = ans;
-            }
+            if (freq[el - 'A'] == mxFreq)
+                ++cnt;
+            else if (freq[el - 'A'] > mxFreq)
+                mxFreq = freq[el - 'A'], cnt = 1;
         }
-        return ans;
+        return max((int)tasks.size(), (mxFreq - 1) * (n + 1) + cnt);
     }
 };
