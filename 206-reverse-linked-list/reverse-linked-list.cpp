a8dd70389a2501408fe2c495cdacf1f8cab10630
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-21 14:29:39 +0200

    Time: 8 ms (16.90%) | Memory: 11.6 MB (34.51%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    ListNode *reverseList(ListNode *head) {
+        if (!head)
+            return head;
+        ListNode *curr = head, *next = head->next, *tmp;
+        head->next = NULL;
+        while (next) {
+            tmp = next->next;
+            next->next = curr;
+            curr = next;
+            next = tmp;
+        }
+        return curr;
+    }
+};
