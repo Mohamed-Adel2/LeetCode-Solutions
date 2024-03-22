7dcceada196fe926c745aaa2d9e6cbaf40bd89fe
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-22 15:13:49 +0200

    Time: 208 ms (9.43%) | Memory: 122.9 MB (42.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -3,8 +3,7 @@ public:
     bool isPalindrome(ListNode *head) {
         string s;
         while (head) {
-            s.push_back(head->val);
-            head = head->next;
+            s.push_back(head->val), head = head->next;
         }
         int sz = s.size();
         for (int i = 0; i < sz / 2; ++i)
