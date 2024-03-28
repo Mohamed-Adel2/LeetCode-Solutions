3fbeb53c0386b4f2839904b3c78512a5b0a0761b
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-28 09:30:29 +0200

    Time: 62 ms (36.39%) | Memory: 63.7 MB (13.88%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -6,7 +6,7 @@ public:
         int left = 0, right = -1, ans = 0, curr = 1, sz = nums.size();
         while (++right < sz) {
             curr *= nums[right];
-            while (left <= right && curr >= k)
+            while (curr >= k && left <= right)
                 curr /= nums[left++];
             ans += right - left + 1;
         }
