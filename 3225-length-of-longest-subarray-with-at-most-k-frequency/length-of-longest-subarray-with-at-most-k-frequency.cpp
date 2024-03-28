414da6c24638f1eb8c0ed33dc29a9b346eca9dcb
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-28 09:36:32 +0200

    Time: 163 ms (98.58%) | Memory: 148.4 MB (86.57%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
+    int maxSubarrayLength(vector<int> &nums, int k) {
         unordered_map<int, int> freq;
         int left = 0, right = 0, ans = 0;
-        while(right<nums.size()){
-            ++freq[nums[right]];
-            if(freq[nums[right]]>k){
-                while(left < right && freq[nums[right]] > k)
+        while (right < nums.size()) {
+            if (++freq[nums[right]] > k)
+                while (left < right && freq[nums[right]] > k)
                     --freq[nums[left++]];
-            }
             ++right;
-            ans = max(ans, right-left);
+            ans = max(ans, right - left);
         }
         return ans;
     }
