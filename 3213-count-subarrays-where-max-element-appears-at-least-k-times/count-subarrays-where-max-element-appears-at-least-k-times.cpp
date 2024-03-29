29320726c7fec44c143cf3834205832ccef942bc
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-03-29 05:46:09 +0200

    Time: 131 ms (62.02%) | Memory: 123.6 MB (43.60%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int k) {
+    long long countSubarrays(vector<int> &nums, int k) {
         int mx = 0, sz = nums.size();
         long long ans = 0;
-        for(int i = 0;i<sz;++i)
+        for (int i = 0; i < sz; ++i)
             mx = max(mx, nums[i]);
-        int left = 0, right = 0, cnt = 0;
-        deque<int> el;
-        while(right < sz){
-            if(nums[right] == mx)
-                el.push_back(right), ++cnt;
-            if(cnt == k){
-                long long before = el.front() - left, after = sz - right - 1;
-                ans += before + after + before * after + 1;
-                while(nums[left++] != mx);
-                --cnt;
-                el.pop_front();
-            }
-            ++right;
+        vector<int> el;
+        for (int i = 0; i < sz; ++i)
+            if (nums[i] == mx)
+                el.push_back(i);
+        int elSz = el.size();
+        for (int i = 0; i + k - 1 < elSz; ++i) {
+            long long before = (i ? el[i] - el[i - 1] - 1 : el[i]), after = sz - el[i + k - 1] - 1;
+            ans += before + after + before * after + 1;
         }
         return ans;
     }
