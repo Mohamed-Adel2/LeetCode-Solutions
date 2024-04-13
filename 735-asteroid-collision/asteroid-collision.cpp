e345e4504360faa52b8964ee1210f09485fdd37d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-04-13 19:33:14 +0200

    Time: 10 ms (55.86%) | Memory: 20.6 MB (51.71%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -15,7 +15,7 @@ public:
             }
         }
         vector<int> ans(st.size());
-        for(int i = st.size() - 1;i>=0;--i)
+        for(int i = st.size() - 1;i >= 0;--i)
             ans[i] = st.top(), st.pop();
         return ans;
     }
