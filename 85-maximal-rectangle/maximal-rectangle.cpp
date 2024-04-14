36ca3cac633cdabb873ae7951d824905f60c9efd
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-04-14 16:48:28 +0200

    Time: 41 ms (25.21%) | Memory: 16.5 MB (89.96%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>> &matrix) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<vector<int>> ones(rows, vector<int>(cols));
+        // calculate the longest consecutive ones from each index
+        for (int i = 0; i < rows; ++i) {
+            int next = cols;
+            for (int j = cols - 1; j >= 0; --j) {
+                if (matrix[i][j] == '1')
+                    ones[i][j] = next - j;
+                else
+                    next = j;
+            }
+        }
+        int ans = 0;
+        for (int j = 0; j < cols; ++j) {
+            for (int start = 0; start < rows; ++start) {
+                int mx = 1000;
+                for (int i = start; i < rows; ++i) {
+                    if (matrix[i][j] == '0')
+                        break;
+                    mx = min(mx, ones[i][j]);
+                    ans = max(ans, mx * (i - start + 1));
+                }
+            }
+        }
+        return ans;
+    }
+};
