6deb830fa8ed26204ed9b2fb1ce3d180b8a5d96a
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-04-18 05:29:51 +0200

    Time: 76 ms (51.27%) | Memory: 101.1 MB (29.64%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int ans = 0, n, m;
+    int n, m;
     vector<int> dx{1, -1, 0, 0}, dy{0, 0, 1, -1};
     int islandPerimeter(vector<vector<int>>& grid) {
         n = grid.size(), m = grid[0].size();
-        for(int i = 0;i<grid.size();++i)
-            for(int j = 0;j<grid[0].size();++j)
-                if(grid[i][j] == 1){
-                    solve(i, j, grid);
-                    return ans;
+        int ans = 0;
+        for(int i = 0;i<n;++i)
+            for(int j = 0;j<m;++j)
+                if(grid[i][j]){
+                    for(int k = 0;k<4;++k)
+                        if(!valid(i+dx[k], j+dy[k]) || !grid[i+dx[k]][j+dy[k]])
+                            ++ans;
+
                 }
-        return -18405215;
+        return ans;
     }
-    void solve(int row, int col, vector<vector<int>>& grid){
-        ans += 4;
-        grid[row][col] = -1;
-        for(int i = 0;i<4;++i){
-            int newX = row + dx[i], newY = col + dy[i];
-            if(valid(newX, newY)){
-                if(grid[newX][newY] == 1)
-                    solve(row+dx[i], col+dy[i], grid), ans-=1;
-                else if(grid[newX][newY] == -1)
-                    ans-=1; 
-            }
-        }
-    }
+
     bool valid(int row, int col){
         return(row >= 0 && row < n && col >= 0 && col < m);
     }
