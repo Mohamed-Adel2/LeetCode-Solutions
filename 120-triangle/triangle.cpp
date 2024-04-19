89879f43fe6c3fa01c87d498e64977c87b494f55
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-04-19 15:20:53 +0200

    Time: 3 ms (85.27%) | Memory: 11.3 MB (47.51%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int n, m;
+
+    int minimumTotal(vector<vector<int>> &triangle) {
+        n = triangle.size(), m = triangle[0].size();
+        dp.resize(n + 2, vector<int>(n + 2, 1e9));
+        return solve(0, 0, triangle);
+    }
+
+    int solve(int row, int col, vector<vector<int>> &triangle) {
+        if (row >= n)
+            return 0;
+        int &ret = dp[row][col];
+        if (ret != 1e9)
+            return ret;
+        ret = min(solve(row + 1, col, triangle), solve(row + 1, col + 1, triangle)) + triangle[row][col];
+        return ret;
+    }
+};
