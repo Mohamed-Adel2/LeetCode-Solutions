bebf183f684fc1851b654ac01000f18e2e9fc633
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-05-13 20:54:12 +0300

    Time: 7 ms (73.89%) | Memory: 42.9 MB (45.63%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,28 +2,27 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length())
             return false;
-        Map<Character, Integer> s1Freq = new HashMap<>(), s2Freq = new HashMap<>();
+        int[] s1Freq = new int[26], s2Freq = new int[26];
         for (int i = 0; i < s1.length(); ++i) {
-            s1Freq.put(s1.charAt(i), s1Freq.getOrDefault(s1.charAt(i), 0) + 1);
-            s2Freq.put(s2.charAt(i), s2Freq.getOrDefault(s2.charAt(i), 0) + 1);
+            s1Freq[s1.charAt(i) - 'a']++;
+            s2Freq[s2.charAt(i) - 'a']++;
         }
         if (equal(s1Freq, s2Freq))
             return true;
         int sz = s1.length();
         for (int i = sz; i < s2.length(); ++i) {
-            s2Freq.put(s2.charAt(i - sz), s2Freq.get(s2.charAt(i - sz)) - 1);
-            s2Freq.put(s2.charAt(i), s2Freq.getOrDefault(s2.charAt(i), 0) + 1);
+            s2Freq[s2.charAt(i - sz) - 'a']--;
+            s2Freq[s2.charAt(i) - 'a']++;
             if (equal(s1Freq, s2Freq))
                 return true;
         }
         return false;
     }
 
-    public boolean equal(Map<Character, Integer> s1Freq, Map<Character, Integer> s2Freq) {
-        for (Map.Entry<Character, Integer> pair : s1Freq.entrySet()) {
-            if (!Objects.equals(s2Freq.getOrDefault(pair.getKey(), 0), pair.getValue()))
+    public boolean equal(int[] s1Freq, int[] s2Freq) {
+        for (int i = 0; i < 26; ++i)
+            if (s1Freq[i] != s2Freq[i])
                 return false;
-        }
         return true;
     }
 }
