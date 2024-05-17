ad5ff126bb667874af02bfa8bb0b8ed645f22c82
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-05-17 15:49:42 +0300

    Time: 1759 ms (62.72%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,4 @@
 SELECT contest_id, ROUND(COUNT(contest_id)/(SELECT COUNT(*) FROM USERS)*100 ,2) AS percentage
-
-FROM Users u JOIN Register r
-ON u.user_id = r.user_id
+FROM Register r
 GROUP BY contest_id
 ORDER BY percentage DESC , contest_id ASC
