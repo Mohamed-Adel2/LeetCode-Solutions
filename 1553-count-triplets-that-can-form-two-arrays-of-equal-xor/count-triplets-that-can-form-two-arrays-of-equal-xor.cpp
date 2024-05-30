151690a8fdc0a324a0f79f2158f25ec1ae1faad1
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-05-30 06:07:43 +0300

    Time: 0 ms (100.00%) | Memory: 9.2 MB (49.44%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -3,20 +3,13 @@ public:
     int countTriplets(vector<int> &arr) {
         int n = arr.size();
         vector<int> prefix(n + 1);
-        unordered_map<int, int> freq;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) 
             prefix[i + 1] = prefix[i] ^ arr[i];
-            ++freq[prefix[i + 1]];
-        }
         int ans = 0;
-        for (int i = 0; i < n - 1; ++i) {
-            --freq[prefix[i + 1]];
-            unordered_map<int, int> prevFreq;
-            for (int j = i + 1; j < n; ++j) {
-                ans += freq[prefix[i]] - prevFreq[prefix[i]];
-                ++prevFreq[prefix[j + 1]];
-            }
-        }
+        for (int i = 0; i < n - 1; ++i)
+            for (int k = i + 1; k < n; ++k)
+                if(prefix[i] == prefix[k + 1])
+                    ans += (k - i);
         return ans;
     }
 };
