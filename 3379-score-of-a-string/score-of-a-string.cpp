6f86cda998983bdfb932906d5351224c27bfc44d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-06-01 09:51:50 +0300

    Time: 5 ms (27.27%) | Memory: 7.9 MB (42.17%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfString(string s) {
         int ans = 0;
         for(int i = 0; i < s.size() - 1; ++i)
-            ans += abs(s[i] - s[i+1]);
+            ans += abs(s[i + 1] - s[i]);
         return ans;
     }
 };
