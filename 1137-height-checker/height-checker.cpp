a0e282dde4ac80bcf463eb45828e08a39d3d297b
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-06-10 21:16:12 +0300

    Time: 0 ms (100.00%) | Memory: 10 MB (96.49%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+        int ans = 0;
+        for(int i = 0; i < heights.size(); ++i)
+            ans += (expected[i] != heights[i]);
+        return ans;
+    }
+};
