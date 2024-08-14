3c0f881c51d8c5dedf25956d4e71bc77ad7bb356
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 21:04:53 +0300

    Time: 725 ms (37.46%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -3,6 +3,6 @@ SELECT
     SUM(unit) AS unit
 FROM Orders o, Products p
 WHERE o.product_id = p.product_id
-AND DATE_FORMAT(order_date, '%Y-%m') = '2020-02'
+AND DATE_FORMAT(order_date, '%M %Y') = 'February 2020'
 GROUP BY p.product_id
 HAVING SUM(unit) >= 100
