1ab8ce69169510ef56f63a3738b658372863d39f
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 13:55:03 +0300

    Time: 989 ms (65.12%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,10 +1,11 @@
+WITH cte AS (
+    SELECT *, RANK() over (PARTITION BY product_id ORDER BY year) AS rnk
+    FROM Sales
+)
 SELECT 
     product_id,
     year AS first_year,
     quantity,
     price
-FROM (
-    SELECT *, RANK() over (PARTITION BY product_id ORDER BY year) AS rnk
-    FROM Sales
-) AS s
-WHERE s.rnk = 1;
+FROM cte
+WHERE cte.rnk = 1;
