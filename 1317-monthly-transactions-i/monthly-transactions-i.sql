aacda167d85ad3c1030e95dec0142643bd107b9a
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 10:48:41 +0300

    Time: 770 ms (15.46%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -2,9 +2,9 @@ SELECT
     DATE_FORMAT(trans_date, '%Y-%m') AS month,
     country,
     COUNT(*) AS trans_count,
-    (SELECT COUNT(*) FROM Transactions t WHERE DATE_FORMAT(t.trans_date, '%Y-%m') = month && (tt.country = t.country || (t.country IS NULL && tt.country IS NULL)) && state = 'approved') AS approved_count,
+    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    COALESCE((SELECT SUM(amount) FROM Transactions t WHERE DATE_FORMAT(t.trans_date, '%Y-%m') = month && (tt.country = t.country || (t.country IS NULL && tt.country IS NULL)) && state = 'approved'), 0) AS approved_total_amount
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
     
 FROM Transactions tt
 GROUP BY country, month
