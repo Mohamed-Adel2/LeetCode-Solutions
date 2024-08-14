56f2b9721e6fbf3af762e90e41106495e2f88a3a
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 16:09:54 +0300

    Time: 1494 ms (34.81%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,14 @@
 With cte AS (
-    SELECT person_name,
-    SUM(weight) OVER (ORDER BY turn) AS w
+    SELECT 
+        person_name,
+        turn,
+        SUM(weight) OVER (ORDER BY turn) AS w
     FROM Queue
 )
 
 SELECT
     person_name
 FROM cte
-WHERE cte.w IN (SELECT MAX(w) FROM cte WHERE cte.w <= 1000);
+WHERE cte.w <= 1000
+ORDER BY turn DESC
+LIMIT 1;
