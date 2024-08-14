4f459fde4329a0db541203237de6fcf4d24235c5
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 12:22:15 +0300

    Time: 1280 ms (6.87%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,10 @@
-SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT customer_id) FROM delivery) * 100, 2) AS immediate_percentage
-FROM Delivery d1
-WHERE order_date = (
-    SELECT MIN(order_date)
-    FROM Delivery d2
-    WHERE d1.customer_id = d2.customer_id
-) AND order_date = customer_pref_delivery_date;
+SELECT ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0)) / COUNT(*) * 100, 2) AS immediate_percentage
+FROM (
+    SELECT *
+    FROM Delivery d1
+    WHERE order_date = (
+        SELECT MIN(order_date)
+        FROM Delivery d2
+        WHERE d1.customer_id = d2.customer_id
+    )
+) AS res;
