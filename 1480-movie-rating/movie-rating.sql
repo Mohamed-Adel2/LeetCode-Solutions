70dcf24b65dc9dc0e54bc0efd4e7bdf2d07cdaee
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 17:55:28 +0300

    Time: 1266 ms (33.71%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,31 @@
+WITH cte AS (
+    SELECT
+        name,
+        COUNT(*) AS cnt
+    FROM MovieRating m, Users u
+    WHERE m.user_id = u.user_id
+    GROUP BY u.user_id, name
+    ORDER BY cnt DESC, name ASC
+    LIMIT 1
+),
+cte2 AS(
+    SELECT
+        title,
+        AVG(rating) AS average
+    FROM MovieRating m, Movies mv
+    WHERE m.movie_id = mv.movie_id
+    AND DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+    GROUP BY mv.movie_id, title
+    ORDER BY average DESC, title ASC
+    LIMIT 1    
+)
+
+SELECT
+    name AS results
+FROM cte
+
+UNION ALL
+
+SELECT
+    title AS results
+FROM cte2
