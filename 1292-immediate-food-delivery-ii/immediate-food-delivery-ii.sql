9441365add32fb3aa3b495dcce9d823ed294050f
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 12:18:18 +0300

    Time: 1265 ms (7.01%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,7 @@
-SELECT ROUND((SELECT COUNT(*) FROM Delivery d1 WHERE order_date = (SELECT MIN(order_date) FROM Delivery d2 WHERE d1.customer_id = d2.customer_id) AND order_date = customer_pref_delivery_date) / (SELECT COUNT(*)
+SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT customer_id) FROM delivery) * 100, 2) AS immediate_percentage
 FROM Delivery d1
 WHERE order_date = (
     SELECT MIN(order_date)
     FROM Delivery d2
     WHERE d1.customer_id = d2.customer_id
-)) * 100, 2) AS immediate_percentage;
+) AND order_date = customer_pref_delivery_date;
