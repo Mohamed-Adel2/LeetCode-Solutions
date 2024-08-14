9ecef4f46654cfeb07b53b7edf77e5b88dd4d6be
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 15:58:15 +0300

    Time: 506 ms (37.36%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,6 +1,6 @@
 SELECT
     product_id,
-    IF(change_date <= '2019-08-16', new_price, 10) AS price
+    new_price AS price
 FROM Products
 WHERE (product_id, change_date) IN (
     SELECT product_id, MAX(change_date)
@@ -8,9 +8,8 @@ WHERE (product_id, change_date) IN (
     WHERE change_date <= '2019-08-16'
     GROUP BY product_id
 )
-OR (product_id, change_date) IN (
-    SELECT product_id, MIN(change_date)
-    FROM Products
-    GROUP BY product_id
-    HAVING MIN(change_date) > '2019-08-16'
-)
+UNION
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
