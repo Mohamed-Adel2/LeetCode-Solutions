1578667b26c40378a08f0553b134ea5721de1127
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 15:24:00 +0300

    Time: 521 ms (53.33%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,14 @@
-SELECT employee_id, department_id 
-FROM Employee
-WHERE primary_flag = 'Y'
-UNION
-SELECT employee_id, department_id 
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(employee_id) = 1;
+WITH cte AS(
+    SELECT 
+        employee_id,
+        SUM(IF(primary_flag = 'Y', 1, 0)) AS y,
+        SUM(IF(primary_flag = 'N', 1, 0)) AS n
+    FROM Employee
+    GROUP BY employee_id
+)
+SELECT
+    e.employee_id,
+    e.department_id
+FROM Employee e, cte c
+WHERE e.employee_id = c.employee_id
+AND (e.primary_flag = 'Y' OR (c.n = 1 AND c.y = 0));
