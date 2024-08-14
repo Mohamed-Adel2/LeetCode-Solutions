1f15042a9b4277d300e5009928f05d6c9fe2ee4b
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-14 13:40:34 +0300

    Time: 433 ms (83.96%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,6 @@
-SELECT activity_date as day, COUNT(distinct user_id) as active_users
-FROM
-    Activity 
-WHERE activity_date <= '2019-07-27' AND activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY)
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users 
+FROM Activity
+WHERE activity_date between DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
 GROUP BY activity_date;
