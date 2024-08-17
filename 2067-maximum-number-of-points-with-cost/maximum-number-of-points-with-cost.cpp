5afbd8f85a83b5abc4de5a20edd348a7092445cf
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-17 06:23:40 +0300

    Time: 206 ms (46.32%) | Memory: 117.2 MB (44.96%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    long long maxPoints(vector<vector<int>> &points) {
+    long long maxPoints(vector<vector<int>>& points) {
         int n = points.size(), m = points[0].size();
-        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(m + 1, vector<long long>(4, -1)));
-        return solve(0, 0, n, m, 0, dp, points);
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, -1));
+        return solve(0, 0, n, m, dp, points);
     }
 
-    long long
-    solve(int row, int col, int n, int m, int dir, vector<vector<vector<long long>>> &dp, vector<vector<int>> &points) {
-        if (row >= n || col >= m || row < 0 || col < 0)
+    long long solve(int row, int col, int n, int m, vector<vector<long long>>& dp, vector<vector<int>>& points){
+        if(row >= n || col >= m || row < 0 || col < 0)
             return 0;
-        long long &ret = dp[row][col][dir];
-        if (~ret)
+        long long& ret = dp[row][col];
+        if(~ret)
             return ret;
-        ret = solve(row + 1, col, n, m, 0, dp, points) + points[row][col];
-        if (dir != 1)
-            ret = max(ret, solve(row, col - 1, n, m, 2, dp, points) - (row ? 1 : 0));
-        if (dir != 2)
-            ret = max(ret, solve(row, col + 1, n, m, 1, dp, points) - (row ? 1 : 0));
+        ret = solve(row + 1, col, n, m, dp, points) + points[row][col];
+        ret = max(ret, solve(row, col - 1, n , m, dp, points) - (row ? 1 : 0));
+        ret = max(ret, solve(row, col + 1, n , m, dp, points) - (row ? 1 : 0));
         return ret;
     }
+
 };
