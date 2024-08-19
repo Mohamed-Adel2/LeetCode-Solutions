bf6e04a3063122686f5a4d20ba5adf2de8c8724d
Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>
2024-08-19 13:43:26 +0300

    Time: 4 ms (33.17%) | Memory: 8 MB (63.87%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> dp(s.size() + 2, -1);
+        return solve(0, s, dp);
+    }
+
+    int solve(int idx, string& s, vector<int>& dp){
+        if(idx >= s.size())
+            return 1;
+        if (s[idx] == '0')
+            return 0;
+        int& ret = dp[idx];
+        if(~ret)
+            return ret;
+        ret = solve(idx + 1, s, dp);
+        if(idx + 1 < s.size() && (s[idx] == '1' || (s[idx] == '2' && s[idx + 1] <= '6')))
+            ret += solve(idx + 2, s, dp);
+        return ret;
+    }
+};
